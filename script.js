e4f9c5399b72a332a8a000d0dcba8ff566e56de0
Vitor Souza <vitor.souza@absoftware.com.br>
2023-05-07 18:09:40 -0300

    corrigindo calculo

--- script.js
+++ script.js
@@ -8,26 +8,26 @@ function calcularImc(){
         aviso.innerHTML = "Dado Inválido";
         aviso.className = "aviso-erro";
     }else{
-        var resultado = peso / (altura*altura);
+        var resultado = Math.floor(peso / (altura*altura));
         console.log(resultado);
 
         if(resultado >= 0 && resultado < 19){
-            aviso.innerHTML = "Você está abaixo do peso: " + Math.floor(resultado);
+            aviso.innerHTML = "Você está abaixo do peso: " + resultado;
             aviso.className = "aviso-abaixo";
         }else if(resultado >=19 && resultado <=24){
-            aviso.innerHTML = "Você está no Peso Ideal: " +  Math.floor(resultado);
+            aviso.innerHTML = "Você está no Peso Ideal: " + resultado;
             aviso.className = "aviso-ideal";
         }else if(resultado >=25 && resultado <=29){
-            aviso.innerHTML = "Você está com Sobrepeso: " +  Math.floor(resultado);
+            aviso.innerHTML = "Você está com Sobrepeso: " +  resultado;
             aviso.className = "aviso-sobrepeso";
         }else if(resultado >=30 && resultado <=34){
-            aviso.innerHTML = "Você está em Obesidade Grau I: " +  Math.floor(resultado);
+            aviso.innerHTML = "Você está em Obesidade Grau I: " +  resultado;
             aviso.className = "aviso-obs1";
         }else if(resultado >=35 && resultado <=39){
-            aviso.innerHTML = "Você está em Obesidade Grau II: " +  Math.floor(resultado);
+            aviso.innerHTML = "Você está em Obesidade Grau II: " + resultado;
             aviso.className = "aviso-obs2";
         }else if(resultado >=40){
-            aviso.innerHTML = "Você está em Obesidade Grau III: " +  Math.floor(resultado);
+            aviso.innerHTML = "Você está em Obesidade Grau III: " +  resultado;
             aviso.className = "aviso-obs3";
         }else{
             aviso.innerHTML = "Classificação Não Existente";
